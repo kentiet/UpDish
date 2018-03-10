a4d2bf1eadd4e9b0677716299147e7515730df4a
mijeonglee86 <mjlee9286@naver.com>
2018-03-09 17:04:01 -0800

    DetailActivity - Modified XML - Slides

--- app/src/main/java/com/example/ken/updish/MainActivity.java
+++ app/src/main/java/com/example/ken/updish/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ken.updish;
 
+import android.content.Intent;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(MainActivity.this, DetailActivity.class));
     }
 }
