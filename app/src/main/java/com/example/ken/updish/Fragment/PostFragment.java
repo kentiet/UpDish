489c4cea78dcb2fed438fc0d56c17f32e0b5e015
mijeonglee86 <mjlee9286@naver.com>
2018-03-26 18:11:25 -0700

    [X] working on post fragment

--- app/src/main/java/com/example/ken/updish/Fragment/PostFragment.java
+++ app/src/main/java/com/example/ken/updish/Fragment/PostFragment.java
@@ -2,7 +2,12 @@ package com.example.ken.updish.Fragment;
 
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -17,19 +22,26 @@ import android.widget.Toast;
 
 import com.example.ken.updish.R;
 
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PostFragment extends Fragment {
+    public static final int IMAGE_GALLERY_REQUEST = 20;
 
     Activity context;
     private TextView mTextMessage;
+    private ArrayList<Bitmap> bitmapArray = new ArrayList<>();
 
     public PostFragment() {
         // Required empty public constructor
         Log.e("Updish", "Test fragment constructor", null);
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,13 +51,50 @@ public class PostFragment extends Fragment {
         context = (Activity)getActivity();
 
         Button btn_new = (Button)view.findViewById(R.id.btn_Newpost);
+        final Button btn_picture = (Button)view.findViewById(R.id.btn_picture);
+
         btn_new.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(context,"Clicked",Toast.LENGTH_LONG).show();
             }
         });
+        btn_picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Gallery button clicked", Toast.LENGTH_LONG).show();
+                onImageGalleryClicked(btn_picture);
+            }
+        });
 
         return view;
+    }
+    public void onImageGalleryClicked(View view){
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String pictureDirectoryPath = pictureDirectory.getPath();
+        Uri data = Uri.parse(pictureDirectoryPath);
+
+        photoPickerIntent.setDataAndType(data, "image/*");
+        startActivityForResult(photoPickerIntent, IMAGE_GALLERY_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            InputStream inputStream;
+
+            try{
+                //inputStream = getContentResolver().openInputStream(imageUri);
+                Bitmap image = BitmapFactory.decodeStream(inputStream);
+                Bitmap resizedBitmap = Bitmap.createScaledBitmap(image, 100, 100, false);
+
+                bitmapArray.add(resizedBitmap);
+
+                bitmapArray
+            }
+        }
     }
 }
